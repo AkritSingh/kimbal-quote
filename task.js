b3af8a536a43d8bf37a187b6eb7d7ac2c6026b15
Akrit Singha <akrit.singha11@gmail.com>
2025-11-11 01:15:59 +0530

    stable

---
+++ task.js
@@ -0,0 +1,298 @@
+tasks = [
+    {
+        title: "UI/UX Design",
+        completion: 'not started',
+        breakdown: [
+            {
+                title: "What,s New Page Creation",
+                completion: 'not started',
+                breakdown: [
+                    {
+                        title: "Page Creation",
+                        price: 4000,
+                        days: 2,
+                        completion: 'not started',
+                    },
+                    {
+                        title: "Easy CMS-level publishing",
+                        price: 2000,
+                        days: 2,
+                        completion: 'not started',
+                    },
+                    {
+                        title: "Update navigation",
+                        price: 1000,
+                        days: 1,
+                        completion: 'not started',
+                    }
+                ]
+            },
+            {
+                title: "Content Updates",
+                price: 5000,
+                days: 3,
+                completion: 'not started',
+            },
+            {
+                title: "Dynamic Page for Resource",
+                price: 10000,
+                days: 5,
+                completion: 'not started',
+            }
+        ]
+    },
+    {
+        title: "QC Testing (Before)",
+        completion: 'not started',
+        breakdown: [
+            {
+                title: "Linking Validation",
+                price: 3000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Performance Testing",
+                price: 1500,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Functionality Testing",
+                price: 1500,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "User Experience Testing",
+                price: 1500,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Device Compatibility Testing",
+                price: 1500,
+                days: 1,
+                completion: 'not started',
+            }
+            
+        ]
+    },
+    {
+        title: "Versioning",
+        completion: 'not started',
+        breakdown: [
+            {
+                title: "Plugin Version Update",
+                price: 5000,
+                days: 2,
+                completion: 'not started',
+            },
+            {
+                title: "Plugin Errors Fix",
+                price: 3000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Unused Plugin Remove",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Plugin Compatibility Check",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Wordpress Version Update",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "PHP Version Update",
+                price: 4000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Perform Stability Check",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            }
+        ]
+    },
+    {
+        title: "Performance",
+        completion : "not started",
+        breakdown: [
+            {
+                title: "Extra js removal",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Minify CSS",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            
+            {
+                title: "Minify JavaScript",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Handle Animation Smoothness",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Lazy-load third-party resources",
+                price: 4000,
+                days: 2,
+                completion: 'not started',
+            },
+            {
+                title: "Defer offscreen images",
+                price: 3000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Fix resource blocking",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Minimize main-thread work",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Image Optimization",
+                price: 3000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Additional Code Optimization.",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+
+
+
+        ]
+    },
+    {
+        title: "On-Page SEO Optimization",
+        completion: 'not started',
+        breakdown: [
+            {
+                title: "Best Practices",
+                price: 5000,
+                days: 2,
+                completion: 'not started',
+            },
+            {
+                title: "Improve Web Vitals",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Keywords relevancy check",
+                price: 1000,
+                days: 1,
+                completion: 'not started',
+            },
+        ]
+    },
+    {
+        title: "QC Testing (After)",
+        completion: 'not started',
+        breakdown: [
+            {
+                title: "Linking Validation",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Performance Testing",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Functionality Testing",
+                price: 2000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "User Experience Testing",
+                price: 1000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Device Compatibility Testing",
+                price: 1000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "Final Review",
+                completion: 'not started',
+                breakdown: [
+                    {
+                        title: "Overall Performance Comparison",
+                        price: 1000,
+                        days: 1,
+                        completion: 'not started',
+                    },
+                    {
+                        title: "Overall User Experience Comparison",
+                        price: 2000,
+                        days: 1,
+                        completion: 'not started',
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        title: "Documentation",
+        completion: 'not started',
+        breakdown: [
+            {
+                title: "Make easily accessable",
+                price: 1000,
+                days: 1,
+                completion: 'not started',
+            },
+            {
+                title: "User friendly UI",
+                price: 1000,
+                days: 1,
+                completion: 'not started',
+            }
+        ]
+    },
+    
+    
+]
+
