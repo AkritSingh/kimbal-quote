be083d751c77e69e598c11aa26268b22e07bed11
Akrit Singha <akrit.singha11@gmail.com>
2026-03-13 15:40:42 +0530

    ok

--- task.js
+++ task.js
@@ -1,23 +1,23 @@
 tasks = [
     {
         title: "Security Fixes",
-        completion: 'not started',
+        completion: 'completed',
         breakdown: [
             {
                 title: "Exposed phpinfo() page (Information Disclosure)",
-                completion: 'not started',
+                completion: 'completed',
                 price: 5000,
                 days: 2,
             },
             {
                 title: "Deprecated SSH 8.0 on port 2222",
-                completion: 'not started',
+                completion: 'completed',
                 price: 5000,
                 days: 2,
             },
             {
                 title: "Missing anti-clickjacking headers",
-                completion: 'not started',
+                completion: 'completed',
                 price: 5000,
                 days: 2,
             },
@@ -25,31 +25,31 @@ tasks = [
                 title: "Server banner discloses nginx/1.29.4",
                 price: 5000,
                 days: 2,
-                completion: 'not started',
+                completion: 'completed',
             },
             {
                 title: "WordPress version disclosure via wp-links-opml.php",
                 price: 5000,
                 days: 2,
-                completion: 'not started',
+                completion: 'completed',
             },
             {
                 title: "Add reCAPTCHA script, which user should check after submit. (Google reCAPTCHA v2)",
                 price: 5000,
                 days: 2,
-                completion: 'not started',
+                completion: 'completed',
             },
             {
                 title: "Add Limitor of 5 times post comment in a day.",
                 price: 5000,
                 days: 3,
-                completion: 'not started',
+                completion: 'completed',
             },
             {
                 title: "Detect comment with some blacklisted keywords and delete them automatically.",
                 price: 2000,
                 days: 1,
-                completion: 'not started',
+                completion: 'completed',
             }
 
         ]
@@ -88,7 +88,6 @@ tasks = [
                 days: 4,
                 completion: 'not started',
             }
-
         ]
     }
 ]
